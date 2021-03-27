1ac0be8705213757614f65d7d4c7df341f2cbb79
Ahmed Bilal <56721456+ahmedbilal1510@users.noreply.github.com>
2021-03-27 18:36:49 +0900

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+
+    Public Function CountChars(ByVal inputText As String) As Integer
+        Return inputText.Length
+    End Function
+
+    Public Function CpuntWords(ByVal inputText As String) As Integer
+        If (inputText = "") Then Return 0
+
+        Dim stringWords() As String = inputText.Split(" ".ToCharArray())
+        Return stringWords.Length
+    End Function
+
+    Public Sub UpdateGui()
+        Dim inputText As String = txtWords.Text
+        Dim outputText As String
+
+        If (radCountWords.Checked) Then
+            outputText = CountWords(inputText) & " words"
+        Else
+            outputText = CountChars(inputText) & " characters"
+        End If
+        lblOutput.Text = outputText
+
+    End Sub
+
+    Private Sub txtWords_TextChanged(sender As Object, e As EventArgs) Handles txtWords.TextChanged
+        lblOutput.Text = CountChars(txtWords.Text) & " characters"
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        End
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
